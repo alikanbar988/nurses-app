47b4b987d7638700cf0bc2dad1f8685e1a828742
=ali <=>
2023-11-12 14:27:36 +0200

    edit in welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome to my app ');
+    return view('welcome to my app this the best application');
 });
 
 Route::get('rejester',function()'){
